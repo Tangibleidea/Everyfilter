db4dd54a992a7f3860e59a9b20c90cc0597ff83a
Mark Choi <mark.choi@tangibleidea.net>
2020-03-06 00:43:02 +0900

    function to get service of Google sheet.

--- everyfilter.py
+++ everyfilter.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
 import tldextract # pip install tldextract
-import os.path
 import pickle
+import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -8,9 +9,17 @@ import datetime
 import sys
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-def getService():
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1Pg30XVEQtW2dkhtmCR2uIJW8CfJhPzyvEJ1KO110CTM'
+SAMPLE_RANGE_NAME = 'top_3000_south_korea_traffic_new!A:A'
+
+
+def getSheetService():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -23,12 +32,30 @@ def getService():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('abptools_clientsecret.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('drive', 'v3', credentials=creds)
+    service = build('sheets', 'v4', credentials=creds)
     return service
+
+def main():
+    service = getSheetService()
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            #print('%s, %s' % (row[0], row[4]))
+            print(row[0])
+
+if __name__ == '__main__':
+    main()
