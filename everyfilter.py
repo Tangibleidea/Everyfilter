bc6886eb26f8378e1b6802ec5413a9a20ae812b2
Mark Choi <mark.choi@tangibleidea.net>
2020-03-08 01:36:09 +0900

    save/open current status itself.

--- everyfilter.py
+++ everyfilter.py
@@ -1,16 +1,22 @@
 from __future__ import print_function
 import tldextract # pip install tldextract
 import pickle
-import os.path
+from os import path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import datetime
 import sys
-
+import ssl
+import requests.api
+import re
+import validators
+import urllib3
+from urllib.request import urlopen
+import time
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
- 
+
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '16XngdfajuFrCzi_fIUr6kyNiRRfVM_aVQfIKk38G4Oc'
 service = None
@@ -59,9 +65,12 @@ def getSheetService():
 
 
 def readSheet(sheetName):
-    #service = getSheetService()
+    global service
     #Call the Sheets API
     rangeName= sheetName= "!A:Z"
+
+    if service is None:
+        service = getSheetService()
 
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=rangeName).execute()
@@ -78,11 +87,91 @@ def readSheet(sheetName):
                 except IndexError:
                     break
 
+
+
+def readSource3(url):
+    arrSource= []   
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    data = response.data.decode('utf-8')
+    for line in data.splitlines():
+        arrSource.append(line)
+    print('done')
+    return arrSource
+
+def readSource1(url):
+    arrSource= []   
+    data = urlopen(url)
+    for source in data:
+        arrSource.append(source)
+    print('done')
+
+def savePickle(target):
+    with open('dump.filters', 'wb') as f:
+        pickle.dump(target, f)
+
+def openPickle():
+    with open('dump.filters', 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+def saveTXT(target):
+    with open('dump.filters', 'w') as f:
+        for item in target:
+            f.write("%s\n" % item)
+
+def openTXT():
+    arrLines= []
+    with open("C:\name\MyDocuments\numbers") as file:
+        for line in file:
+            line = line.strip() #preprocess line
+            arrLines.append(line)
+    return arrLines
+
 def main():
     global service
-    arrSheet= getSheetList()
-    for sheet in arrSheet:
-        readSheet(sheet)
+
+    start_time = time.time()
+    if path.exists("dump.filters"):
+        opened= openTXT()
+        print(opened)
+    else:
+        arrSource= readSource3("https://easylist-downloads.adblockplus.org/easylist.txt")
+        saveTXT(arrSource)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+    # if validators.domain('http://stackoverflow.com/a/30007882/697313'):
+    #     print('this domain is valid')
+    # else:
+    #     print('this domain is not valid')
+
+    # pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+    #test_string1 = 'https://stackoverflow.com/a/30007882/697313'
+    # test_string2 = '0.018321'
+    # extracted = tldextract.extract(test_string2)
+    # print(extracted)
+
+    # result1 = re.findall(pattern, test_string1)
+    # print(result1)
+    # result2 = re.findall(pattern, test_string2)
+    # print(result2)
+
+    # if result:
+    #     print("Search successful.")
+    # else:
+    #     print("Search unsuccessful.")	
+
+    # try:
+    #     service = getSheetService()
+    # except requests.exceptions.SSLError:
+    #     print("===SSLError===")
+
+    # arrSheet= getSheetList()
+    # for sheet in arrSheet:
+    #     readSheet(sheet)
+
+    
+    
 
 if __name__ == '__main__':
     main()
